2e2fbfec0da51b5742008efdd385cf3af43d3b70
mudasirhayat <mudasirhayat5@gmail.com>
2025-09-23 05:00:12 +0000

    fix: improve error handling

--- eva-voice/utils/samples.py
+++ eva-voice/utils/samples.py
@@ -5,6 +5,6 @@ try:
     audio_file = str(AUDIO_DIR / "crab-story" / "mono_1.wav")
     story_text = "OK fresh start, how about this... Close your eyes for a second"
     audio_file_2 = str
-    str(AUDIO_DIR / "crab-story" / "mono_3.wav"): "If pip's bouncing off the shell walls with excitement, he probably wouldnt wait for a map or hidden tunnels.  hehehehe.  Hed probably grap that compass, yell Cya! to the other crabs and scurry out into the sand dunes as fast as his little legs could carry him.  ",
-    str(AUDIO_DIR / "crab-story" / "mono_4.wav"): "Love blooms in the strangest places, right?  I bet pip never expected to find the same crab of his dreams, in the middle of a sandstorm.  But, maybe he does?   Or maybe he finds a love as stubborn and adventurous as he is.  Someone who appreciates a good mystery and isn't afraid to get a little sand in her gills.  Where do you see them meeting?  hm?",
+try:
+    str(AUDIO_DIR / "crab-story" / "mono_3.wav"): "If pip's bouncing off the shell walls with excitement, he probably wouldnt wait for a map or hidden tunnels.  hehe
 }
