36e934c90f3cd772942ba01c10a8eb30c59c85ab
mudasirhayat <mudasirhayat5@gmail.com>
2025-10-31 17:00:14 +0000

    fix: improve error handling

--- eva-voice/utils/samples.py
+++ eva-voice/utils/samples.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 
-AUDIO_DIR = Path("wav")
 try:
+    AUDIO_DIR = Path("wav")
     audio_file = str(AUDIO_DIR / "crab-story" / "mono_1.wav")
+except Exception as e:
+    print(f"An error
     story_text = "OK fresh start, how about this... Close your eyes for a second"
     audio_file_2 = str
 try:
