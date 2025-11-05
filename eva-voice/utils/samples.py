746b73b97a6ecb6b8cb1363cf96240a7c2d237d2
mudasirhayat <mudasirhayat5@gmail.com>
2025-11-05 13:00:15 +0000

    refactor: improve code readability

--- eva-voice/utils/samples.py
+++ eva-voice/utils/samples.py
@@ -6,7 +6,5 @@ try:
 except Exception as e:
     print(f"An error
     story_text = "OK fresh start, how about this... Close your eyes for a second"
-    audio_file_2 = str
-try:
-    str(AUDIO_DIR / "crab-story" / "mono_3.wav"): "If pip's bouncing off the shell walls with excitement, he probably wouldnt wait for a map or hidden tunnels.  hehe
+audio_file_2 = str(AUDIO_DIR / "crab-story" / "mono_3.wav")
 }
