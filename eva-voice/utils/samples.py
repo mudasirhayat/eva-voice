fbe7ba018ecd381b3f96021a94c2e1c09610e58e
mudasirhayat <mudasirhayat5@gmail.com>
2025-11-11 09:00:14 +0000

    refactor: improve code readability

--- eva-voice/utils/samples.py
+++ eva-voice/utils/samples.py
@@ -4,7 +4,6 @@ try:
     AUDIO_DIR = Path("wav")
     audio_file = str(AUDIO_DIR / "crab-story" / "mono_1.wav")
 except Exception as e:
-    print(f"An error
-    story_text = "OK fresh start, how about this... Close your eyes for a second"
+story_text = "OK fresh start, how about this... Close your eyes for a second"
 audio_file_2 = str(AUDIO_DIR / "crab-story" / "mono_3.wav")
 }
