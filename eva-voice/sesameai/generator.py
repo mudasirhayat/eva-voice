d2df92c525232733d32c8e453637bde7582fe96f
mudasirhayat <mudasirhayat5@gmail.com>
2025-11-19 09:00:14 +0000

    refactor: improve code readability

--- eva-voice/sesameai/generator.py
+++ eva-voice/sesameai/generator.py
@@ -21,10 +21,8 @@ class Segment:
 
 def load_llama3_tokenizer():
     """
-    https://github.com/huggingface/transformers/issues/22794#issuecomment-2092623992
-    """
-    tokenizer_name = "meta-llama/Llama-3.2-1B"
-    tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
+tokenizer_name = "meta-llama/Llama-3.2-1B"
+tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
 try:
     bos = tokenizer.bos_token
     eos = tokenizer.eos_token
