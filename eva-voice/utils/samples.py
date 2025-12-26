7696810707579b6ec86307a65d121438b3c21895
mudasirhayat <mudasirhayat5@gmail.com>
2025-12-26 05:00:14 +0000

    fix: improve error handling

--- eva-voice/utils/samples.py
+++ eva-voice/utils/samples.py
@@ -4,9 +4,5 @@ try:
     AUDIO_DIR = Path("wav")
     audio_file = str(AUDIO_DIR / "crab-story" / "mono_1.wav")
 except Exception as e:
-    print("An error occurred:", e)
     raise e
-
-try:
-    story_text = "OK fresh start, how about this... Close your eyes for a second
 }
